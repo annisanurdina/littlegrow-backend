2d5df6791caac2858e026b0ea46e2b68643f46c7
annisanurdina <89742238+annisanurdina@users.noreply.github.com>
2023-12-19 22:38:11 +0700

    Update handler.js
    revisi handler

--- src/handler.js
+++ src/handler.js
@@ -1,11 +1,10 @@
-const input = require('./connection');
 const connection = require('./connection');
 
 const addData = (req, res) => {
-  const { nama_anak, jenis_kelamin, berat_badan, tinggi_badan, umur, riwayat_penyakit, jarak_kelahiran } = req.body; // Ganti dengan nama kolom yang sesuai di tabel Anda
-  const insertQuery = 'INSERT INTO input (nama_anak, jenis_kelamin, berat_badan, tinggi_badan, umur, riwayat_penyakit, jarak_kelahiran) VALUES (?, ?, ?, ?, ?, ?, ?)'; // Ganti dengan nama tabel dan kolom yang sesuai
+  const { nama_anak, jenis_kelamin, berat_badan, tinggi_badan, umur, riwayat_penyakit, jarak_kelahiran, lingkar_lengan, lingkar_kepala, status_user, user_id, profile_id } = req.body; // Ganti dengan nama kolom yang sesuai di tabel Anda
+  const insertQuery = 'INSERT INTO input (nama_anak, jenis_kelamin, berat_badan, tinggi_badan, umur, riwayat_penyakit, jarak_kelahiran, lingkar_lengan, lingkar_kepala, status_user, user_id, profile_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'; // Ganti dengan nama tabel dan kolom yang sesuai
 
-  connection.query(insertQuery, [nama_anak, jenis_kelamin, berat_badan, tinggi_badan, umur, riwayat_penyakit, jarak_kelahiran], (err, result) => {
+  connection.query(insertQuery, [nama_anak, jenis_kelamin, berat_badan, tinggi_badan, umur, riwayat_penyakit, jarak_kelahiran, lingkar_lengan, lingkar_kepala, status_user, user_id, profile_id], (err, result) => {
     if (err) {
       console.error('Gagal menambahkan data:', err);
       res.status(500).json({ error: 'Gagal menambahkan data' });
@@ -30,11 +29,41 @@ const getData = (req, res) => {
   });
 };
 
-const deleteData = (req, res) => {
-  const { nama_anak } = req.params; // Ganti dengan parameter yang sesuai dengan kebutuhan Anda
-  const deleteQuery = 'DELETE FROM input WHERE nama_anak = ?'; // Ganti dengan nama tabel dan kolom yang sesuai
+const getByProfileId = (req, res) => {
+    const profile_id = req.params.profileId;
+    const selectQuery = 'SELECT * FROM input WHERE profile_id = ?'; // Ganti dengan nama tabel yang sesuai
+  
+    connection.query(selectQuery, [profile_id], (err, results) => {
+      if (err) {
+        console.error('Gagal mengambil data:', err);
+        res.status(500).json({ error: 'Gagal mengambil data' });
+        return;
+      }
+      console.log('Data berhasil diambil:', results);
+      res.status(200).json(results);
+    });
+  };
+  
+const getByUserId = (req, res) => {
+    const user_id = req.params.userId;
+    const selectQuery = 'SELECT * FROM input WHERE user_id = ?'; // Ganti dengan nama tabel yang sesuai
+  
+    connection.query(selectQuery, [user_id], (err, results) => {
+      if (err) {
+        console.error('Gagal mengambil data:', err);
+        res.status(500).json({ error: 'Gagal mengambil data' });
+        return;
+      }
+      console.log('Data berhasil diambil:', results);
+      res.status(200).json(results);
+    });
+  };
 
-  connection.query(deleteQuery, [nama_anak], (err, result) => {
+  const deleteData = (req, res) => {
+  const profile_id = req.params.profileId; // Ganti dengan parameter yang sesuai dengan kebutuhan Anda
+  const deleteQuery = 'DELETE FROM input WHERE profile_id = ?'; // Ganti dengan nama tabel dan kolom yang sesuai
+
+  connection.query(deleteQuery, [profile_id], (err, result) => {
     if (err) {
       console.error('Gagal menghapus data:', err);
       res.status(500).json({ error: 'Gagal menghapus data' });
@@ -46,10 +75,11 @@ const deleteData = (req, res) => {
 };
 
 const updateData = (req, res) => {
-  const { nama_anak, jenis_kelamin, berat_badan, tinggi_badan, umur, riwayat_penyakit, jarak_kelahiran } = req.body;
-  const updateQuery = 'UPDATE input SET jenis_kelamin=?, berat_badan=?, tinggi_badan=?, umur=?, riwayat_penyakit=?, jarak_kelahiran=? WHERE nama_anak=?';
+  const {profile_id} = req.params;
+  const { nama_anak, jenis_kelamin, berat_badan, tinggi_badan, umur, riwayat_penyakit, jarak_kelahiran, lingkar_lengan, lingkar_kepala, status_user, user_id } = req.body;
+  const updateQuery = 'UPDATE input SET nama_anak=?, jenis_kelamin=?, berat_badan=?, tinggi_badan=?, umur=?, riwayat_penyakit=?, jarak_kelahiran=?, lingkar_lengan=?, lingkar_kepala=?, status_user=?, user_id=?  WHERE profile_id=?';
 
-  connection.query(updateQuery, [jenis_kelamin, berat_badan, tinggi_badan, umur, riwayat_penyakit, jarak_kelahiran, nama_anak], (err, result) => {
+  connection.query(updateQuery, [nama_anak, jenis_kelamin, berat_badan, tinggi_badan, umur, riwayat_penyakit, jarak_kelahiran, lingkar_lengan, lingkar_kepala, status_user, user_id, profile_id], (err, result) => {
     if (err) {
       console.error('Gagal memperbarui data:', err);
       res.status(500).json({ error: 'Gagal memperbarui data' });
@@ -60,10 +90,11 @@ const updateData = (req, res) => {
   });
 };
 
-
 module.exports = {
     addData,
     getData,
+    getByProfileId,
+    getByUserId,
     deleteData,
     updateData
 };
